af39f1acf00da351cc6e9dc3eb6fcc025fb6cc35
Moaaz-i <famile.sherif@gmail.com>
2025-07-23 18:08:42 +0300

    delete-file:h/html

---
+++ assets/js/typed.js
@@ -0,0 +1,8 @@
+var typed = new Typed("#typedText", {
+  strings: ["developer", "freelancer", "photographer", "designer"],
+  typeSpeed: 60,
+  backSpeed: 70,
+  loop: true,
+  showCursor: true,
+  smartBackspace: true,
+});
